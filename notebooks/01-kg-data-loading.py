8baab3a36522788ae88307947bd587c9d4aacf40
Kalin Gibbons <kalingibbons@boisestate.edu>
2022-09-13 16:31:40 -0600

    Update data-loading starter notebook

--- notebooks/01-kg-data-loading.py
+++ notebooks/01-kg-data-loading.py
@@ -1,6 +1,7 @@
 # %%
 from pathlib import Path
 
+import pandas as pd
 import pyvista as pv
 
 from fepredict import __file__ as filestr
@@ -10,6 +11,11 @@ module_dir = Path(filestr).parents[2]
 del filestr
 
 
+# %% [markdown]
+# # Import the mesh
+# I put a subset of the real data in `data/raw`. You'll need to update your environment
+# with the new environment.yml file before the pyvista script works.
+
 # %%
 raw_data_dir = module_dir.joinpath("data", "raw")
 simulation_dir = raw_data_dir / "9905863M00"
@@ -17,10 +23,46 @@ simulation_dir = raw_data_dir / "9905863M00"
 tibia_cartilage_lateral = pv.read_meshio(
     simulation_dir / "TIB_CART_LAT_G.inp", file_format="abaqus"
 )
-tibia_cartilage_lateral.plot()
+tibia_cartilage_lateral.plot(jupyter_backend="static")
 
 # %% [markdown]
-# # Data loading gameplan
+# # Import the contact pressure data
+#
+# The columns are frame number, just think of as time or "percent of knee flexion
+# activity". The rows are node numbers.
+#
+# > **Note**: Abaqus users and Clare will refer to them as
+# >           nodes and elements, but pyvista uses points and cells.
+
+cpress_tib_cart_lateral = pd.read_pickle(
+    simulation_dir / "CPRESS_TIB_CART_LAT_NODES.pckl",
+    compression={"method": "gzip", "compresslevel": 1, "mtime": 1},  # important line
+)
+cpress_tib_cart_lateral
+
+# %% [markdown]
+# Contact pressure is a surface output, so the data will not include the interior nodes.
+
+# %%
+tibia_cartilage_lateral.n_points  # includes interior nodes
+
+# %%
+cpress_tib_cart_lateral.shape[0]  # only includes surface
+
+# %% [markdown]
+# To get a time series from this data, for easier machine learning implementations, we
+# can do the maximum contact pressure.
+
+# %%
+cpress_tib_cart_lateral.max(axis=0).plot()
+
+# %% [markdown]
+# Working with these time series would give you enough information to find
+# outliers/failed simulations, and you could start a machine learning pipeline before
+# worrying about contour plots.
+
+# %% [markdown]
+# # Advanced data loading gameplan
 
 # 1. Create point_data array mapping node numbers from _TIB_CART_LAT_G.inp_ to the 0-based
 #    indexes of `tibia_cartilage_lateral`.
